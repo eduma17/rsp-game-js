71a82b995a10f87ec07d5252a73037fd2c599b5b
Eduma17 <dr.roder@gmail.com>
2021-10-19 11:56:02 +0200

    js game function implemented

--- myscript.js
+++ myscript.js
@@ -5,6 +5,37 @@ function computerPlay() {
 
 }
 
+function gameRound (compPlay, gamerPlay) {
+    if (gamerPlay.toLowerCase() === compPlay) {
+        return "Even!";
+        }
+    else if (gamerPlay.toLowerCase() === "rock") {
+        if (compPlay === "siccers") {
+            return "You win!";
+            }
+               
+        else {
+            return "You loose!";
+            }   
+        }
+    else if (gamerPlay.toLowerCase() === "siccers") {
+        if (compPlay === "paper") {
+            return "You win!";
+            }
+        else {
+            return "You loose!";
+            }
+        }
+    else if (gamerPlay.toLowerCase() === "paper") {
+        if (compPlay === "rock") {
+            return "You win!";
+        }
+        else {
+            return "You loose!";
+            }
+        }    
+}
 const cp = computerPlay();
-console.log(cp);
-
+const gp = "Rock";
+console.log("Comp Play:" + cp);
+console.log(gameRound(cp, gp));
